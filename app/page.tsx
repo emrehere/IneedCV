17c6561b8326023413b7270206b03ca4adf91aaf
emrehere <emrah.himself@hotmail.com>
2024-02-08 09:45:41 +0300

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -2,111 +2,71 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="flex justify-betweeen items-center flex-row">
+        <div >
+         <h1>Online Resume Builder</h1>
+         <p>Only 2% of resumes make it past the first round. Be in the top 2%</p>
+         <p>Use professional field-tested resume templates that follow the exact ‘resume rules’ employers look for. Easy to use and done within minutes - try now for free!</p>
+          <button>Create My Resume</button>
+          <p><span className="bg-green-500 h-1 w-1"></span> 39,521 resumes created today</p>
+          <img src="./resume1.jpg" alt="resume1" />
         </div>
+        <div>
+          <p>Reviewed by the community. Trusted by professionals</p>
+          <span>4.5 out of 5</span>
+          <p>4,5 yildiz yap</p>
+          <p>1 yildiz TrustGO</p>
+          <p>based on 48,877 reviews</p>
+        </div>
+        <div>
+          <p>CV bana cok yardimci ...</p>
+          <span>CV oluşturmak hiç bu kadar kolay olmamıştı! Bu platform, şık şablonları ve kullanıcı dostu arayüzü ile CV yazma sürecini oldukça basitleştiriyor. Çok memnun kaldım!</span>
+        </div>
+        <div>
+          <p> CV oluşturmak gerçekten hızlı ve etkili. Şablon seçenekleri ve düzenleme araçları sayesinde özgeçmişimi kişiselleştirmek çok kolay oldu.</p>
+          <p>özelleştirilebilir şablonları ve kullanıcı dostu arayüzü sayesinde CV'yi kişiselleştirmek hiç bu kadar keyifli olmamıştı. Kesinlikle tavsiye ederim!</p>
+          <p>CV oluşturmak hiç bu kadar eğlenceli olmamıştı! Görsel düzenleme seçenekleri ve şık tasarımları sayesinde özgeçmişim tam istediğim gibi oldu.</p>
+          <p>profil bilgilerini kullanarak anında bir özgeçmiş oluşturmanıza olanak tanıyor. Hızlı, pratik ve işe yarıyor!</p>
+          <p>CV oluşturmak için kullanılan bu platform, sadece profesyonel değil aynı zamanda yaratıcı bir görünüm sunuyor. Daha önce denediğim en iyi CV oluşturucu!</p>
+          <p>Özgeçmişimi oluşturmak için kullanılan bu araç, hem modern şablonlarıyla hem de kullanım kolaylığıyla dikkat çekiyor. Kesinlikle denemelisiniz!</p>
+          <p>CV oluşturmak için tercih ettiğim platform, özelleştirme seçenekleri ve çeşitli şablonlarıyla gerçekten öne çıkıyor. Hem ücretsiz seçenekleri var hem de premium özellikleri mevcut.</p>
+          <p>CV oluşturucusu olarak tercih ettiğim bu araç, sadece şık tasarımları değil aynı zamanda kolay kullanımıyla da öne çıkıyor. İş arama sürecimi oldukça kolaylaştırdı.</p>
+        </div>
+        <div>
+            <div>Easy online resume builder
+Create an awesome resume in minutes, without leaving your web browser.</div>
+<div>Approved templates
+Professionally-designed resume templates and examples. Just edit and download in 5 minutes.</div>
+        </div>
+        <div>Optimized resumes
+Formats and designs are optimized for resume-filtering algorithms. Ensure humans see your application!</div>
+   
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <p>"Şık ve hazır kullanıma uygun özgeçmiş şablonları
+25'ten fazla zarif, profesyonel tasarlanmış özgeçmiş şablonumuzdan birini kullanarak işverenleri ve işe alım uzmanlarını etkileyin. Word veya PDF olarak indirin."</p>
+        <button>Select Template</button>
+        <p>4,5 yildiz</p>
+        <p>
+4.5 out of 5
+based on 48,877 reviews on Trustpilot</p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <p>Frequently Asked Questions</p>
+        <p>Özgeçmişimi PDF olarak indirebilir miyim? Özgeçmişiniz hazır olduğunda, iş başvurularına başlamak için özgeçmişinizi veya önyazınızı dışa aktarmanın birkaç yolu vardır. Özgeçmişinizi doğrudan Kontrol Panelinizden veya Özgeçmiş Düzenleyiciden PDF, DOCX (Word) veya TXT dosyası olarak indirebilirsiniz. Daha fazlasını bu makalede öğrenin.</p>
+        <p>Özgeçmişimi nasıl özelleştirebilirim? Özgeçmiş şablonlarımız, içeriğinize uyum sağlamak ve tüm tasarımlarımızda harika görünmek üzere tasarlanmıştır.</p>
+        <p>Şifremi nasıl sıfırlayabilirim? Eğer şifrenizi unuttuysanız, giriş sayfasını ziyaret ederek 'Şifremi Unuttum' veya 'Şifreyi Sıfırla' bağlantısını tıklayabilirsiniz. E-postanıza gönderilen talimatları takip ederek yeni bir şifre oluşturabilir ve hesabınıza yeniden erişim sağlayabilirsiniz.</p>
+      </div>
+      <div>Join over 34,149,000 users worldwide</div>
+      <div>
+        <p>Your First Steps</p>
+        <p>We’ve made sure that signing up to our resume maker tools is even more convenient than usual. Use one of the most common networks used by professionals (LinkedIn, Facebook or your Google account) or simply skip this step and enter your name and email address. We keep your data strictly confidential.</p>
+        <p>Achieve Beauty With Ease</p>
+        <p>Choose one of our beautiful, professionally designed resume or cover letter formats. Add your personal info and choose and edit the necessary sections. Customize the layout and visuals as much (or as little) as you want. We provide a ton of ready content with lots of room for your own creativity and needs.</p>
+        <p>Now It’s Yours!</p>
+        <p>Export your new resume, CV or application letter in one of the available formats. PDF will provide you with the best and most consistent visual formatting. Word files allow you to edit the document further or submit the resume to an online application system. You can also share your career updates online.</p>
       </div>
     </main>
   );
