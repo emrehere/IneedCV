ed31638915f1289613fcda98be21810e3ddf9c9f
emrehere <emrah.himself@hotmail.com>
2024-02-08 12:46:12 +0300

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -3,71 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-betweeen items-center flex-row">
-        <div >
-         <h1>Online Resume Builder</h1>
-         <p>Only 2% of resumes make it past the first round. Be in the top 2%</p>
-         <p>Use professional field-tested resume templates that follow the exact ‘resume rules’ employers look for. Easy to use and done within minutes - try now for free!</p>
-          <button>Create My Resume</button>
-          <p><span className="bg-green-500 h-1 w-1"></span> 39,521 resumes created today</p>
-          <img src="./resume1.jpg" alt="resume1" />
-        </div>
-        <div>
-          <p>Reviewed by the community. Trusted by professionals</p>
-          <span>4.5 out of 5</span>
-          <p>4,5 yildiz yap</p>
-          <p>1 yildiz TrustGO</p>
-          <p>based on 48,877 reviews</p>
-        </div>
-        <div>
-          <p>CV bana cok yardimci ...</p>
-          <span>CV oluşturmak hiç bu kadar kolay olmamıştı! Bu platform, şık şablonları ve kullanıcı dostu arayüzü ile CV yazma sürecini oldukça basitleştiriyor. Çok memnun kaldım!</span>
-        </div>
-        <div>
-          <p> CV oluşturmak gerçekten hızlı ve etkili. Şablon seçenekleri ve düzenleme araçları sayesinde özgeçmişimi kişiselleştirmek çok kolay oldu.</p>
-          <p>özelleştirilebilir şablonları ve kullanıcı dostu arayüzü sayesinde CV'yi kişiselleştirmek hiç bu kadar keyifli olmamıştı. Kesinlikle tavsiye ederim!</p>
-          <p>CV oluşturmak hiç bu kadar eğlenceli olmamıştı! Görsel düzenleme seçenekleri ve şık tasarımları sayesinde özgeçmişim tam istediğim gibi oldu.</p>
-          <p>profil bilgilerini kullanarak anında bir özgeçmiş oluşturmanıza olanak tanıyor. Hızlı, pratik ve işe yarıyor!</p>
-          <p>CV oluşturmak için kullanılan bu platform, sadece profesyonel değil aynı zamanda yaratıcı bir görünüm sunuyor. Daha önce denediğim en iyi CV oluşturucu!</p>
-          <p>Özgeçmişimi oluşturmak için kullanılan bu araç, hem modern şablonlarıyla hem de kullanım kolaylığıyla dikkat çekiyor. Kesinlikle denemelisiniz!</p>
-          <p>CV oluşturmak için tercih ettiğim platform, özelleştirme seçenekleri ve çeşitli şablonlarıyla gerçekten öne çıkıyor. Hem ücretsiz seçenekleri var hem de premium özellikleri mevcut.</p>
-          <p>CV oluşturucusu olarak tercih ettiğim bu araç, sadece şık tasarımları değil aynı zamanda kolay kullanımıyla da öne çıkıyor. İş arama sürecimi oldukça kolaylaştırdı.</p>
-        </div>
-        <div>
-            <div>Easy online resume builder
-Create an awesome resume in minutes, without leaving your web browser.</div>
-<div>Approved templates
-Professionally-designed resume templates and examples. Just edit and download in 5 minutes.</div>
-        </div>
-        <div>Optimized resumes
-Formats and designs are optimized for resume-filtering algorithms. Ensure humans see your application!</div>
-   
-
-      </div>
-      <div>
-        <p>"Şık ve hazır kullanıma uygun özgeçmiş şablonları
-25'ten fazla zarif, profesyonel tasarlanmış özgeçmiş şablonumuzdan birini kullanarak işverenleri ve işe alım uzmanlarını etkileyin. Word veya PDF olarak indirin."</p>
-        <button>Select Template</button>
-        <p>4,5 yildiz</p>
-        <p>
-4.5 out of 5
-based on 48,877 reviews on Trustpilot</p>
-      </div>
-      <div>
-        <p>Frequently Asked Questions</p>
-        <p>Özgeçmişimi PDF olarak indirebilir miyim? Özgeçmişiniz hazır olduğunda, iş başvurularına başlamak için özgeçmişinizi veya önyazınızı dışa aktarmanın birkaç yolu vardır. Özgeçmişinizi doğrudan Kontrol Panelinizden veya Özgeçmiş Düzenleyiciden PDF, DOCX (Word) veya TXT dosyası olarak indirebilirsiniz. Daha fazlasını bu makalede öğrenin.</p>
-        <p>Özgeçmişimi nasıl özelleştirebilirim? Özgeçmiş şablonlarımız, içeriğinize uyum sağlamak ve tüm tasarımlarımızda harika görünmek üzere tasarlanmıştır.</p>
-        <p>Şifremi nasıl sıfırlayabilirim? Eğer şifrenizi unuttuysanız, giriş sayfasını ziyaret ederek 'Şifremi Unuttum' veya 'Şifreyi Sıfırla' bağlantısını tıklayabilirsiniz. E-postanıza gönderilen talimatları takip ederek yeni bir şifre oluşturabilir ve hesabınıza yeniden erişim sağlayabilirsiniz.</p>
-      </div>
-      <div>Join over 34,149,000 users worldwide</div>
-      <div>
-        <p>Your First Steps</p>
-        <p>We’ve made sure that signing up to our resume maker tools is even more convenient than usual. Use one of the most common networks used by professionals (LinkedIn, Facebook or your Google account) or simply skip this step and enter your name and email address. We keep your data strictly confidential.</p>
-        <p>Achieve Beauty With Ease</p>
-        <p>Choose one of our beautiful, professionally designed resume or cover letter formats. Add your personal info and choose and edit the necessary sections. Customize the layout and visuals as much (or as little) as you want. We provide a ton of ready content with lots of room for your own creativity and needs.</p>
-        <p>Now It’s Yours!</p>
-        <p>Export your new resume, CV or application letter in one of the available formats. PDF will provide you with the best and most consistent visual formatting. Word files allow you to edit the document further or submit the resume to an online application system. You can also share your career updates online.</p>
-      </div>
+    
     </main>
   );
 }
