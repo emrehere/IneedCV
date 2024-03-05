55fbe847cb554ed3a3ccadf0097916999e585d2f
emrehere <emrah.himself@hotmail.com>
2024-03-05 18:46:06 +0300

    preparing chooseCV page

---
+++ frontend/app/components/chooseCVbutton.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+
+
+interface Props {
+    hrefSend: string | undefined
+}
+
+function chooseCVbutton({ hrefSend }: Props) {
+    return (
+        <div>
+            <Link href={hrefSend === undefined ? "" : hrefSend}>
+                <button className='bg-red-500 hover:bg-red-600 text-white text-xl w-[22vw] py-2 mx-2 rounded-xl font-bold'>Proceed With This CV</button>
+            </Link>
+        </div>
+    )
+}
+
+export default chooseCVbutton
--- frontend/app/pages/chooseCV/page.tsx
+++ frontend/app/pages/chooseCV/page.tsx
@@ -1,13 +1,62 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import ChooseCVbutton from '@/app/components/chooseCVbutton';
 
 function Page() {
+
+  const [img1, setImg1] = useState("");
+  const [img2, setImg2] = useState("");
+  const [img3, setImg3] = useState("");
+
+  const clickImage = (index: number) => {
+    const normalStyle = ""
+    const clickedStyle = "scale-125 mx-20 mb-12 flex"
+
+    if (index === 1) {
+      setImg1(clickedStyle)
+      setImg2(normalStyle)
+      setImg3(normalStyle)
+    } else if (index === 2) {
+      setImg1(normalStyle)
+      setImg2(clickedStyle)
+      setImg3(normalStyle)
+    }
+    else if (index === 3) {
+      setImg1(normalStyle)
+      setImg2(normalStyle)
+      setImg3(clickedStyle)
+    }
+  }
+
+
+
   return (
     <div>
-      <div>
-        <p>Choose your CV and Get your resume printed in less than 2 minutes</p>
-        <div className='flex justify between mx-auto w-[80vw]'>
-            
+      <p>Choose your CV and Get it printed in less than 2 minutes</p>
+      <div className='w-[100vw]'>
+
+        <div className='flex justify-center mx-auto w-[70vw] mt-20 '>
+          <div>
+            <img key={1} onClick={() => clickImage(1)} className={`${img1} h-[60vh]`} src="/2.webp" alt=" cv 2 " />
+            <div className={img1 === "" ? "hidden" : "flex justify-center "}>
+            <ChooseCVbutton hrefSend="/pages/create2" />
+            </div>
+          </div>
+          <div>
+            <img key={2} onClick={() => clickImage(2)} className={`${img2} h-[62vh] -mt-[2vh]`} src="/1.webp" alt=" cv 1 " />
+            <div className={img2 === "" ? "hidden" : "flex justify-center "}>
+            <ChooseCVbutton hrefSend="/pages/templates" />
+            </div>
+          </div>
+          <div>
+            <img key={3} onClick={() => clickImage(3)} className={`${img3} h-[60vh]`} src="/3.webp" alt=" cv 3 " />
+            <div className={img3 === "" ? "hidden" : "flex justify-center "}>
+            <ChooseCVbutton hrefSend="/pages/create3" />
+            </div>
+           
+          </div>
         </div>
+
       </div>
     </div>
   )
