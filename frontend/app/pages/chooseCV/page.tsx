4d2dbf60fd33992217d4d0912256029bbe38d2e4
emrehere <emrah.himself@hotmail.com>
2024-03-06 19:27:30 +0300

    slider is done

--- frontend/app/pages/chooseCV/page.tsx
+++ frontend/app/pages/chooseCV/page.tsx
@@ -12,6 +12,8 @@ function Page() {
   const [img2, setImg2] = useState("");
   const [img3, setImg3] = useState("");
 
+  const [slideIndex, setSlideIndex] = useState(1);
+
   const { token, setToken} = useInfo()
 
   const router = useRouter()
@@ -48,17 +50,34 @@ function Page() {
     }
   }
 
+  function arrowLeftFunc() {
+    if (slideIndex !== 1) {
+      setSlideIndex(slideIndex - 1)
+    } else {
+      setSlideIndex(3)
+    }
+  }
+
+  function arrowRightFunc() {
+    if (slideIndex !== 3) {
+      setSlideIndex(slideIndex + 1)
+    } else {
+      setSlideIndex(1)
+    }
+  }
+
+  console.log(slideIndex)
 
 
   return (
     <div>
       {
         token && (
-          <div className='bg-[#070717] min-h-screen pb-[5vh] overflow-x-hidden'>
+          <div className='bg-[#070717] min-h-screen pb-[10vh] overflow-x-hidden'>
             <div className='flex justify-center text-2xl sm:text-3xl font-bold my-12 text-white w-[90vw] mx-auto' >
               <p>Choose your CV and Get it printed in less than 2 minutes !</p>
             </div>
-            <div className='w-[100vw] bg-blue-50 sm:h-[90vh] h-[85vh]'>
+            <div className='w-[100vw] bg-blue-50 sm:h-[90vh] h-[90vh]'>
 
               <div className=' hidden sm:flex justify-center mx-auto w-[70vw] pt-[10vh] mt-4 '>
                 <div>
@@ -81,31 +100,33 @@ function Page() {
 
                 </div>
               </div>
+
               <div className=' sm:hidden flex items-center  mx-auto  pt-[10vh]  '>
-                  <div className='text-4xl absolute left-2 text-red-500 '>
-                  <FaArrowAltCircleLeft />
+                  <div className='text-5xl absolute left-2 text-red-500 '>
+                  <FaArrowAltCircleLeft onClick={arrowLeftFunc} />
                   </div>
-                  <div className='hidden'>
-                  <img key={1} onClick={() => clickImage(1)} className={`${img1} w-[90vw] `} src="/2.webp" alt=" cv 2 " />
-                  <div className="flex justify-center ">
+                  <div className= {slideIndex === 1 ?  "flex flex-col" : "hidden" }>
+                  <img className="w-full" src="/2.webp" alt=" cv 2 " />
+                  <div className="flex justify-center">
                     <ChooseCVbutton hrefSend="/pages/create2" />
                   </div>
                 </div>
-                <div>
-                  <img key={2} onClick={() => clickImage(2)} className={`  w-full -mt-[2vh]`} src="/1.webp" alt=" cv 1 " />
+                <div className= {slideIndex === 2 ?  "flex flex-col" : "hidden" } >
+                  <img className="w-full" src="/1.webp" alt=" cv 1 " />
                   <div className="flex justify-center mt-4">
                     <ChooseCVbutton hrefSend="/pages/templates" />
                   </div>
                 </div>
-                <div className='hidden'>
-                  <img key={3} onClick={() => clickImage(3)} className={`${img3} w-[90vw] `} src="/3.webp" alt=" cv 3 " />
+                <div className= {slideIndex === 3 ?  "flex flex-col" : "hidden"  }>
+                  <img  className="w-full" src="/3.webp" alt=" cv 3 " />
                   <div className="flex justify-center ">
                     <ChooseCVbutton hrefSend="/pages/create3" />
                   </div>
                   </div>
-                  <div className='text-4xl flex justify-end w-[90vw] absolute right-2 text-red-500 '>
-                  <FaArrowAltCircleRight />
+                  <div className='text-5xl flex justify-end w-[90vw] absolute right-2 text-red-500 '>
+                  <FaArrowAltCircleRight onClick={arrowRightFunc} />
                   </div>
+                  
               </div>
 
             </div>
