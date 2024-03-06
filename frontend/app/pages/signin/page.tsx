87c519829057b5566ebf2db7e4d302ceb171518d
emrehere <emrah.himself@hotmail.com>
2024-03-06 16:05:33 +0300

    signin mobile adjusted

--- frontend/app/pages/signin/page.tsx
+++ frontend/app/pages/signin/page.tsx
@@ -14,12 +14,12 @@ function Page() {
     const [inputType, setInputType] = useState("password")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    
+
     const { token, setToken } = useInfo()
 
     const router = useRouter()
 
- 
+
 
     const toggleInputType = () => {
         setInputType("text");
@@ -38,42 +38,42 @@ function Page() {
         const mytoken = localStorage.getItem('token') ?? '';
         console.log(mytoken)
         console.log("token checked")
-        setToken(mytoken)   
+        setToken(mytoken)
         if (mytoken || token) {
             router.push('/pages/chooseCV')
-          }
+        }
 
     }, [router, token])
 
-   
 
-   
+
+
 
     const loginUser = async () => {
-      
-        
+
+
         try {
             const res = await fetch('http://localhost:8000/api/login', {
-               
+
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',             
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
                     email,
                     password,
                 }),
             });
-    
+
             if (res.ok) {
                 const data = await res.json();
                 setUserData(data);
                 console.log(data)
-    
+
                 // Store the token securely in localStorage
                 localStorage.setItem('token', JSON.stringify(data.token));
 
-                console.log('User logged in');    
+                console.log('User logged in');
                 router.push('/pages/chooseCV')
             } else {
                 console.log('User not logged in. Status:', res.status);
@@ -92,23 +92,24 @@ function Page() {
     return (
         <div className='h-[105vh] bg-[url("/signin5.webp")] bg-cover '>
             <div className='h-[80vh] w-full flex flex-col'>
-                <div className='flex-grow' ></div>
-                <div className='flex flex-row'>
+                <div className='flex-grow sm:flex hidden' ></div>
+                <div className='flex flex-row mt-[20vh]'>
                     <div className='flex-grow'></div>
-                    <div className=' bg-white shadow-lg text-gray-600  shadow-[#c0a175] h-[22rem] w-[50vw]
-                     rounded-lg mr-[7vw] items-center flex justify-center flex-col'>
-                        <div className='flex flex-col items-center mb-12'>
-                            <h1 className='text-4xl pb-2 tracking-widest font-semibold text-blue-950'>Long Time No See</h1>
-                            <h2 className='text-3xl font-semibold tracking-wide text-blue-950 ' >Where've You Been !</h2>
+                    <div className=' bg-white shadow-lg text-gray-600  shadow-[#c0a175] sm:h-[22rem]  w-[90vw] sm:w-[50vw]
+                     rounded-lg sm:mr-[7vw] mr-[5vw] items-center flex justify-center flex-col py-8 '>
+                        <div className='flex text-2xl flex-col items-center mb-12'>
+                            <h1 className='sm:text-4xl pb-2 tracking-widest font-semibold text-blue-950'>Long Time No See</h1>
+                            <h2 className='sm:text-3xl font-semibold tracking-wide text-blue-950 ' >Where've You Been !</h2>
                         </div>
-                        <div className='flex flex-row w-[35vw] items-center justify-between'>
-                            <p>Email: </p>
-                            <input value={email} onChange={(e) => setEmail(e.target.value)} className='h-10 w-[28vw] border-blue-950 border-2 outline-none border-opacity-25 px-2' type="text" placeholder='your email' />
+                        <div className='flex flex-row sm:w-[35vw] items-center justify-between w-[85vw]'>
+                            <p className='w-28'>Email: </p>
+                            <input value={email} onChange={(e) => setEmail(e.target.value)} className='h-10 sm:w-[28vw] w-full
+                             border-blue-950 border-2 outline-none border-opacity-25 px-2' type="text" placeholder='your email' />
                         </div>
-                        <div className='flex flex-row w-[35vw] items-center justify-between pt-4'>
-                            <p>Password: </p>
-                            <input value={password} onChange={(e) => setPassword(e.target.value)} className='h-10 ml-4 w-[28vw] border-blue-950 border-2 outline-none border-opacity-25 px-2'
-                                type={inputType} placeholder='your password' />
+                        <div className='flex flex-row sm:w-[35vw] w-[85vw] items-center justify-between pt-4'>
+                            <p className='w-28'>Password: </p>
+                            <input value={password} onChange={(e) => setPassword(e.target.value)} className='h-10  w-full sm:w-[28vw]
+                             border-blue-950 border-2 outline-none border-opacity-25 px-2' type={inputType} placeholder='your password' />
                             <div>
                                 <p onClick={toggleInputType} className={`absolute 
                           ${inputType === "password" ? "bg-blue-950" : "bg-white border-4 border-blue-950 border-opacity-50 "}
@@ -117,7 +118,7 @@ function Page() {
 
                         </div>
 
-                        <button onClick={loginUser} className='bg-blue-950 text-white py-2 w-[20vw] mt-8 rounded-md font-semibold text-xl  shadow-gray-300 hover:shadow-xl '>Login</button>
+                        <button onClick={loginUser} className='bg-blue-950 text-white py-2 sm:w-[20vw] w-[35vw] mt-8 rounded-md font-semibold text-xl  shadow-gray-300 hover:shadow-xl '>Login</button>
                     </div>
                 </div>
             </div>
