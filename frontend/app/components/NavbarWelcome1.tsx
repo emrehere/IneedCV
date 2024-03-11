483ac2b864fbad6d7f2641ba5af8e27e1d2666cf
emrehere <emrah.himself@hotmail.com>
2024-03-11 17:54:04 +0300

    mobile adjustments

--- frontend/app/components/NavbarWelcome1.tsx
+++ frontend/app/components/NavbarWelcome1.tsx
@@ -33,28 +33,40 @@ function NavbarWelcome() {
   }
 
   return (
-    <div className='bg-white h-20   w-full border-b-2 border-white bg-opacity-90 text-white '>
+    <div className='bg-white h-16 overflow-x-hidden  w-full border-b-2 border-white bg-opacity-90 text-white '>
       {
         !token ? (
-          <div>
+          <div className='flex flex-row'>
         <Link href={'/pages/signin'}>
-            <button className='hover:bg-white hover:bg-opacity-90 hover:text-[#070717]  bg-[#070717] px-6 sm:px-12 
-             h-20 font-semibold text-xl sm:border-0 border-r-2 border-white border-opacity-20 tracking-wider'>Sign in</button>
+            <button className='hover:bg-white hover:bg-opacity-90 hover:text-[#070717] 
+             bg-[#070717]  h-16 text-sm flex  items-center justify-center w-[50vw]
+             border-r-2 border-white border-opacity-50 font-semibold tracking-wider
+             sm:w-[25vw] sm:text-xl'>Sign in</button>
         </Link>
         <Link href={'/pages/signup'}>
-            <button className='hover:bg-white hover:bg-opacity-90  hover:text-[#070717] bg-[#070717] px-6 sm:px-12
-            h-20 font-semibold text-xl tracking-wider'>Sign up</button>
+            <button className='hover:bg-white hover:bg-opacity-90  hover:text-[#070717]
+             bg-[#070717]   h-16 text-sm flex  items-center justify-center
+             w-[50vw] font-semibold tracking-wider sm:w-[25vw] sm:font-bold  sm:flex-row sm:space-x-2
+             sm:text-xl'>Sign up</button>
         </Link>
       </div>
         ) : (
-          <div>
+          <div className='flex flex-row'>
              <Link href={'/pages/chooseCV'}>
-            <button className='hover:bg-white hover:bg-opacity-90 hover:text-[#070717]  bg-[#070717] px-3 sm:px-12 
-             h-20 font-semibold text-xl tracking-wider'>Choose CV</button>
+            <button className='hover:bg-white hover:bg-opacity-90 hover:text-[#070717] 
+             bg-[#070717]  h-16 text-sm flex  items-center justify-center w-[50vw]
+             border-r-2 border-white border-opacity-50 font-semibold tracking-wider
+             sm:w-[25vw]  sm:text-xl'>
+               Choose CV
+             </button>
         </Link>
-        <Link href={''}>
-            <button onClick={signOut} className='hover:bg-white hover:bg-opacity-90  hover:text-[#070717] bg-[#070717] px-3 sm:px-12
-             h-20 font-semibold text-xl tracking-wider'>Sign out</button>
+        <Link href={'/'}>
+            <button onClick={signOut} className='hover:bg-white hover:bg-opacity-90  hover:text-[#070717]
+             bg-[#070717]   h-16 text-sm flex flex-col items-center justify-center
+             w-[50vw] font-semibold tracking-wider sm:w-[25vw] sm:font-bold  
+             sm:text-xl'>
+               Sign out
+             </button>
         </Link>
           </div>
         )
--- frontend/app/pages/chooseCV/page.tsx
+++ frontend/app/pages/chooseCV/page.tsx
@@ -34,7 +34,7 @@ function Page() {
 
   const clickImage = (index: number) => {
     const normalStyle = ""
-    const clickedStyle = "scale-110 mx-16 mb-12 flex"
+    const clickedStyle = "scale-110 mx-20 mb-6 flex"
 
     if (index === 1) {
       setImg1(clickedStyle)
@@ -75,27 +75,31 @@ function Page() {
     <div>
       {
         token && (
-          <div className='bg-[#070717] min-h-screen pb-[10vh] overflow-x-hidden'>
-            <div className='flex justify-center text-2xl sm:text-3xl font-bold my-12 text-white w-[90vw] mx-auto' >
+          <div className='bg-[#070717] min-h-screen  overflow-x-hidden'>
+            <div className='flex justify-center text-lg sm:text-3xl font-bold my-4 sm:my-12
+             text-white sm:w-[90vw] w-[80vw] mx-auto' >
               <p>Choose your CV and Get it printed in less than 2 minutes !</p>
             </div>
-            <div className='w-[100vw] bg-blue-50 sm:h-[90vh] h-[90vh]'>
+            <div className='w-[100vw] bg-blue-50 h-[75vh]  sm:pt-[10vh] sm:h-[30rem] '>
 
-              <div className=' hidden md:flex justify-center mx-auto  pt-[10vh] mt-4 '>
+              <div className=' hidden sm:flex justify-center mx-auto  mt-4 '>
                 <div className='relative'>
-                <Image  width={270} height={270} key={1} onClick={() => clickImage(1)} className={`${img1} object-cover xl:h-[52vh]  h-[30vh] lg:h-[45vh]`} src="/2.webp" alt=" cv 2 " />
+                  <Image width={215} height={300} key={1} onClick={() => clickImage(1)} className={`${img1} object-cover `}
+                    src="/2.webp" alt=" cv 2 " />
                   <div className={img1 === "" ? "hidden" : "flex justify-center "}>
                     <ChooseCVbutton hrefSend="/pages/create2" />
                   </div>
                 </div>
                 <div className='relative'>
-                <Image  width={270} height={270} key={2} onClick={() => clickImage(2)} className={`${img2} object-cover h-[30vh] xl:h-[52vh] lg:h-[45vh] lg:-mt-[2vh]`} src="/1.webp" alt=" cv 1 " />
+                  <Image width={215} height={300} key={2} onClick={() => clickImage(2)} className={`${img2} object-cover `}
+                    src="/1.webp" alt=" cv 1 " />
                   <div className={img2 === "" ? "hidden" : "flex justify-center "}>
                     <ChooseCVbutton hrefSend="/pages/templates" />
                   </div>
                 </div>
                 <div className='relative'>
-                <Image  width={270} height={270} key={3} onClick={() => clickImage(3)} className={`${img3} object-cover xl:h-[52vh]  h-[30vh] lg:h-[45vh]`} src="/3.webp" alt=" cv 3 " />
+                  <Image width={215} height={300} key={3} onClick={() => clickImage(3)} className={`${img3} object-cover `}
+                    src="/3.webp" alt=" cv 3 " />
                   <div className={img3 === "" ? "hidden" : "flex justify-center "}>
                     <ChooseCVbutton hrefSend="/pages/create3" />
                   </div>
@@ -103,30 +107,30 @@ function Page() {
                 </div>
               </div>
 
-              <div className=' md:hidden flex items-center  mx-auto  pt-[10vh] pb-[10vh] bg-blue-50  '>
-                <div  onClick={arrowLeftFunc} className='text-5xl absolute left-2 text-red-500 '>
+              <div className=' sm:hidden flex items-center  mx-auto  pt-[10vh] pb-[10vh] bg-blue-50  '>
+                <div onClick={arrowLeftFunc} className='text-5xl absolute left-2 text-red-500 '>
                   <FaArrowAltCircleLeft />
                 </div>
                 <div className={slideIndex === 1 ? "flex flex-col relative w-full h-full items-center justify-center" : "hidden"}>
-                <Image  width={270} height={270} className=" object-cover" src="/2.webp" alt=" cv 2 " />
+                  <Image width={280} height={400} className=" object-cover" src="/2.webp" alt=" cv 2 " />
                   <div className="flex justify-center">
                     <ChooseCVbutton hrefSend="/pages/create2" />
                   </div>
                 </div>
                 <div className={slideIndex === 2 ? "relative flex flex-col w-full h-full items-center justify-center" : "hidden"} >
-                <Image width={270} height={270} className="object-cover" src="/1.webp" alt=" cv 1 " />
-                  <div  className="flex justify-center mt-4">
+                  <Image width={280} height={400} className="object-cover" src="/1.webp" alt=" cv 1 " />
+                  <div className="flex justify-center mt-4">
                     <ChooseCVbutton hrefSend="/pages/templates" />
                   </div>
                 </div>
                 <div className={slideIndex === 3 ? "relative flex flex-col w-full h-full items-center justify-center" : "hidden"}>
-                <Image width={270} height={270} className="object-cover  " src="/3.webp" alt=" cv 3 " />
-                  <div  className="flex justify-center ">
+                  <Image width={280} height={400} className="object-cover  " src="/3.webp" alt=" cv 3 " />
+                  <div className="flex justify-center ">
                     <ChooseCVbutton hrefSend="/pages/create3" />
                   </div>
                 </div>
                 <div onClick={arrowRightFunc} className='text-5xl flex justify-end w-[90vw] absolute right-2 text-red-500 '>
-                  <FaArrowAltCircleRight  />
+                  <FaArrowAltCircleRight />
                 </div>
 
               </div>
